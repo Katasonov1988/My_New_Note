95c7306eb848b89b88ac8bfcd64bc738e0c58cf5
Vladimir Katasonov <katasonovvn@mail.ru>
2022-11-10 22:25:08 +0300

    Created Second Activity xml

---
+++ app/src/main/java/com/example/mynewnote/presentation/NoteItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynewnote.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynewnote.R
+
+class NoteItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_item)
+    }
+}
